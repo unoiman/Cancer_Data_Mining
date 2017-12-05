944809ec9870dd0c2a0c21a487203b6b3c216086
motibz <motibz@mobileye.com>
2017-12-05 15:36:32 +0200

    Model comparing

--- tools/import_data.py
+++ tools/import_data.py
@@ -8,7 +8,7 @@ def load_dataset(source_file = ''):
         data = data.split(',')
         ncols = len(data)
         # print (data[ncols-1])
-    lable_set = np.genfromtxt(source_file, delimiter=",", skip_header=1, usecols= ncols -1 , dtype=str ,max_rows=3)
-    names_set = np.genfromtxt(source_file, delimiter=",", skip_header=1, usecols= 0 , dtype=str,max_rows=3 )
-    faetures_set = np.genfromtxt(source_file, delimiter=",",skip_header=1, usecols=range(1, ncols - 2),max_rows=3)
+    lable_set = np.genfromtxt(source_file, delimiter=",", skip_header=1, usecols= ncols -1 , dtype=str)
+    names_set = np.genfromtxt(source_file, delimiter=",", skip_header=1, usecols= 0 , dtype=str)
+    faetures_set = np.genfromtxt(source_file, delimiter=",",skip_header=1, usecols=range(1, ncols - 2))
     return faetures_set , lable_set ,names_set
